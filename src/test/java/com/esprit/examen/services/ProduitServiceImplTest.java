d4302bc6e340629f6692d892460f569abcc872b0
EmnaFerchichi <ferchichi.emna.dev@gmail.com>
2022-11-14 10:59:36 +0300

    Unit Test Update

--- src/test/java/com/esprit/examen/services/ProduitServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/ProduitServiceImplTest.java
@@ -1,105 +1,80 @@
 package com.esprit.examen.services;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 import com.esprit.examen.entities.Produit;
 import com.esprit.examen.entities.Stock;
 import com.esprit.examen.repositories.ProduitRepository;
 import com.esprit.examen.repositories.StockRepository;
-//@SpringBootTest
-@ExtendWith(SpringExtension.class)
-class ProduitServiceImplTest {
-	
 
+@ExtendWith(SpringExtension.class)
+public class ProduitServiceImplTest {
+	
 	@InjectMocks
 	ProduitServiceImpl produitService;
 	
-	@InjectMocks
-	StockServiceImpl stockService;
-	
-	
-	DateFormat df = new SimpleDateFormat("MM-dd-yyyy"); 
     @Mock
     private ProduitRepository produitRepository;
+    
+    @Mock
     private StockRepository stockRepository;
     
-    private Produit produit1 = new Produit(null,"777434","libbPorduit", 10, new Date(), new Date(),null,null,null);
-    private Produit produit2 = new Produit(null,"777434","libbPorduit", 10, new Date(), new Date(),null,null,null);
+    private Produit product1 = new Produit(null,"125784","libelleProd1", 12, new Date(), new Date(),null,null,null);
+    private Produit product2 = new Produit(null,"365425","libelleProd2", 12, new Date(), new Date(),null,null,null);
+    private Stock stock =new Stock();
 	
-    private Stock stock = new Stock(Long.valueOf(1), "f1", 10, 5, null);
-    
-    @Test 
-	 void testRetrieveAllProduits() {
-	    	when(produitRepository.findAll()).thenReturn(Stream
-	    			.of(produit1,produit2)
-	    			.collect(Collectors.toList()));
-	    	
-	    	assertEquals(2,produitService.retrieveAllProduits().size());
-	    	System.out.println("Retrieve all Produit works !");
-	    }
-    
+	
 	@Test 
-	void testAddProduit() {
-    	when(produitRepository.save(produit1)).thenReturn(produit1);
-    	assertNotNull(produit1);
+	public void givenValidProduct_whenAddProduct_thenSaveProductSuccessfully() {
+    	when(produitRepository.save(product1)).thenReturn(product1);
     	
-    	Produit p = produitService.addProduit(produit1);
-		assertEquals(produit1, p); 
+    	Produit persistedProduct = produitService.addProduit(product1);
     	
-		System.out.println("add product works !");
+		assertEquals(product1, persistedProduct); 
 	} 
-	
-	  @Test 
-	   void testDeleteProduit() {
-		  produitService.deleteProduit(produit1.getIdProduit());
-		  Mockito.verify(produitRepository).deleteById(produit1.getIdProduit());
-		  System.out.println("deleted");
-	    }
-	
-	
-	
-	   @Test 
-	   void testUpdateProduit() {
-	    	when(produitRepository.save(produit1)).thenReturn(produit1);
-	    	assertNotNull(produit1);
-	    	assertEquals(produit1, produitService.updateProduit(produit1));
-	    	System.out.println("Update produit works!");
-	    }
-	    
-	    @Test
-	    void testRetrieveProduit() {
-	    	when(produitRepository.findById(produit1.getIdProduit())).thenReturn(Optional.of(produit1));
-	    	assertEquals(produit1, produitService.retrieveProduit(produit1.getIdProduit()));
-	    	System.out.println("Retrieve product by id works !");
-	    }
-	    
-//	    @Test 
-//		   void testAssignProduitToStock() {
-//	    	when(produitRepository.findById(produit1.getIdProduit())).thenReturn(Optional.of(produit1));
-//	    	assertEquals(produit1, produitService.retrieveProduit(produit1.getIdProduit()));
-//	    	when(stockRepository.findById(stock.getIdStock())).thenReturn(stock);
-//			  assertNotNull(produit1);
-//			  ((ProduitServiceImpl) Mockito.verify(produitRepository)).assignProduitToStock(produit1.getIdProduit(),stock.getIdStock());
-//			  System.out.println("deleted");
-//		    }
-	  
+	@Test 
+    public void whenRetrieveAllProducts_thenReturnTheListOfAllProducts() {
+    	List<Produit> listOfProducts=new ArrayList<Produit>();
+    	listOfProducts.add(product1);
+    	listOfProducts.add(product2);
 
-    
-	
+		when(produitRepository.findAll()).thenReturn(listOfProducts);
+    	
+    	assertEquals(2,produitService.retrieveAllProduits().size());
+    	assertEquals(listOfProducts,produitService.retrieveAllProduits());
+    }
+    @Test 
+    public void givenProductToUpdate_whenUpdateProduct_thenUpdateProductSuccessfully() {
+    	when(produitRepository.save(product1)).thenReturn(product1);
+    	
+    	assertEquals(product1, produitService.updateProduit(product1));
+    }
+    @Test
+    public void givenProductId_whenRetrieveProduct_thenReturnProductSuccessfully() {
+    	when(produitRepository.findById(product1.getIdProduit())).thenReturn(Optional.of(product1));
+    	
+    	assertEquals(product1, produitService.retrieveProduit(product1.getIdProduit()));
+    }
+    @Test
+    public void givenProductAndStockId() {
+    	when(produitRepository.findById(product1.getIdProduit())).thenReturn(Optional.of(product1));
+    	when(stockRepository.findById(stock.getIdStock())).thenReturn(Optional.of(stock));
+    	product1.setStock(stock);
+    	when(produitRepository.save(product1)).thenReturn(product1);
+    	
+    	produitService.assignProduitToStock(product1.getIdProduit(), stock.getIdStock());
+    	
+    	verify(produitRepository,times(1)).findById(product1.getIdProduit());
+    	verify(stockRepository,times(1)).findById(stock.getIdStock());
+    	verify(produitRepository,times(1)).save(product1);
+    }
 }
